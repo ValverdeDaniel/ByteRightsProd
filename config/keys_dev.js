5c6580fc6871c8a956bd5ef01a0f8cd883877358
Valverde Daniel <valverdedaniel44@gmail.com>
2019-09-18 08:26:17 -0600

    updated facebook keys for BrDev

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -2,7 +2,7 @@ module.exports = {
   mongoURI: 'mongodb://danny:danny123@ds253537.mlab.com:53537/brtest',
   googleClientID: '682997216522-arpf59hvmhjpcbgnv11lnshfa5mrtmma.apps.googleusercontent.com',
   googleClientSecret: 'cVLowijK8LQIviV5qkKzbx3P',
-  facebookClientID: '2407546645991029',
-  facebookClientSecret: '8c09d7fe836b2b24b24c529fd74739a8'
+  facebookClientID: '1664378730362805',
+  facebookClientSecret: 'fb199748ca64f9e50f4c7f984c3909cb'
 }
 
