608949b55c5d455a56c1d725a9928726994938e2
elvin solano <elvin.solano@pwc.com>
2019-09-18 21:41:11 -0400

    fixes footer, mobile footer

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,3 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(elems);
 });
-
-console.log("hello");
