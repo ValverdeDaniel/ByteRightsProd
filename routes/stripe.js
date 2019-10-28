6aa5997ae3e68a0cafa7c89225daa70c327790d8
Valverde Daniel <valverdedaniel44@gmail.com>
2019-10-28 07:03:17 -0600

    updated stripe.js route to only contain what is being used

--- routes/stripe.js
+++ routes/stripe.js
@@ -113,136 +113,138 @@ router.get('/token', ensureAuthenticated, async (req, res, next) => {
   }
 });
 
+// start of This section is currently not getting used I used this when I was going off of rocket rides template
 /**
  * GET /pilots/stripe/dashboard
  *
  * Redirect to the pilots' Stripe Express dashboard to view payouts and edit account details.
  */
-router.get('/stripeDashboard', ensureAuthenticated, async (req, res) => {
-  //i believe this is where I am leaving off i successfully got here but then it didn't like my api key
-  console.log('made it to stripe/stripeDashboard')
-  const user = req.user;
-  // Make sure the logged-in pilot completed the Express onboarding
-  if (!user.stripeAccountId) {
-    console.log('!user.stripeAccountId')
-    console.log(user)
-    return res.redirect('/dashboard');
-  }
-  try {
-    // Generate a unique login link for the associated Stripe account to access their Express dashboard
-    console.log('attempting to loginLink')
-    console.log('stripe ' + stripe)
-    console.log('user.stripeAccountId' + user.stripeAccountId);
-    console.log('stripeClientId ' + config.stripe.clientId)
-    const loginLink = await stripe.accounts.createLoginLink(
-      user.stripeAccountId, {
-        //was pilots/dashboard
-        //i want this to take me to stripeDashboard
-        redirect_url: 'http://localhost:5000/dashboard'
-      }
-    );
-    // Directly link to the account tab
-    console.log('req.query.account')
-    if (req.query.account) {
-      loginLink.url = loginLink.url + '/dashboard';
-    }
-    // Retrieve the URL from the response and redirect the user to Stripe
-    return res.redirect(loginLink.url);
-  } catch (err) {
-    console.log(err);
-    console.log('Failed to create a Stripe login link.');
-    return res.redirect('/pilots/signup');
-  }
-});
+// router.get('/stripeDashboard', ensureAuthenticated, async (req, res) => {
+//   //i believe this is where I am leaving off i successfully got here but then it didn't like my api key
+//   console.log('made it to stripe/stripeDashboard')
+//   const user = req.user;
+//   // Make sure the logged-in pilot completed the Express onboarding
+//   if (!user.stripeAccountId) {
+//     console.log('!user.stripeAccountId')
+//     console.log(user)
+//     return res.redirect('/dashboard');
+//   }
+//   try {
+//     // Generate a unique login link for the associated Stripe account to access their Express dashboard
+//     console.log('attempting to loginLink')
+//     console.log('stripe ' + stripe)
+//     console.log('user.stripeAccountId' + user.stripeAccountId);
+//     console.log('stripeClientId ' + config.stripe.clientId)
+//     const loginLink = await stripe.accounts.createLoginLink(
+//       user.stripeAccountId, {
+//         //was pilots/dashboard
+//         //i want this to take me to stripeDashboard
+//         redirect_url: 'http://localhost:5000/dashboard'
+//       }
+//     );
+//     // Directly link to the account tab
+//     console.log('req.query.account')
+//     if (req.query.account) {
+//       loginLink.url = loginLink.url + '/dashboard';
+//     }
+//     // Retrieve the URL from the response and redirect the user to Stripe
+//     return res.redirect(loginLink.url);
+//   } catch (err) {
+//     console.log(err);
+//     console.log('Failed to create a Stripe login link.');
+//     return res.redirect('/pilots/signup');
+//   }
+// });
 
-/**
- * POST /pilots/stripe/payout
- *
- * Generate an instant payout with Stripe for the available balance.
- */
-router.post('/payout', ensureAuthenticated, async (req, res) => {
-  const pilot = req.user;
-  try {
-    // Fetch the account balance to determine the available funds
-    const balance = await stripe.balance.retrieve({
-      stripe_account: pilot.stripeAccountId,
-    });
-    // This demo app only uses USD so we'll just use the first available balance
-    // (Note: there is one balance for each currency used in your application)
-    const {amount, currency} = balance.available[0];
-    // Create an instant payout
-    const payout = await stripe.payouts.create(
-      {
-        amount: amount,
-        currency: currency,
-        statement_descriptor: config.appName,
-      },
-      {
-        stripe_account: pilot.stripeAccountId,
-      }
-    );
-  } catch (err) {
-    console.log(err);
-  }
-  res.redirect('/pilots/dashboard');
-});
+// /**
+//  * POST /pilots/stripe/payout
+//  *
+//  * Generate an instant payout with Stripe for the available balance.
+//  */
+// router.post('/payout', ensureAuthenticated, async (req, res) => {
+//   const pilot = req.user;
+//   try {
+//     // Fetch the account balance to determine the available funds
+//     const balance = await stripe.balance.retrieve({
+//       stripe_account: pilot.stripeAccountId,
+//     });
+//     // This demo app only uses USD so we'll just use the first available balance
+//     // (Note: there is one balance for each currency used in your application)
+//     const {amount, currency} = balance.available[0];
+//     // Create an instant payout
+//     const payout = await stripe.payouts.create(
+//       {
+//         amount: amount,
+//         currency: currency,
+//         statement_descriptor: config.appName,
+//       },
+//       {
+//         stripe_account: pilot.stripeAccountId,
+//       }
+//     );
+//   } catch (err) {
+//     console.log(err);
+//   }
+//   res.redirect('/pilots/dashboard');
+// });
 
 
-//generate a payment
-router.post('/stripeTransaction', ensureAuthenticated, async (req, res, next) => {
-  console.log('reached stripeTransaction');
-  // const sender = req.user;
-  // // Find a random passenger
-  // const sender = await Sender.getRandom();
-  // Create a new ride for the pilot and this random passenger
-  console.log('receiver: ' + req.body.receiver);
-  console.log('sender: '+ req.user.id);
-  console.log('amount: '+ req.body.amount);
-  let newStripeTransaction = new stripeTransaction({
-    receiver: req.body.receiver,
-    sender: req.user.id,
-    // Generate a random amount between $10 and $100 for this ride
-    amount: req.body.amount
-  });
-  console.log('made it through newStripeTransaction')
-  // Save the ride
-  await stripeTransaction(newStripeTransaction).save();
-  try {
-    // Get a test source, using the given testing behavior
-    let source;
-    if (req.body.immediate_balance) {
-      source = getTestSource('immediate_balance');
-    } else if (req.body.payout_limit) {
-      source = getTestSource('payout_limit');
-    }
-    // Create a charge and set its destination to the pilot's account
-    const charge = await stripe.charges.create({
-      source: source,
-      amount: stripeTransaction.amount,
-      currency: stripeTransaction.currency,
-      description: config.appName,
-      statement_descriptor: config.appName,
-      // The destination parameter directs the transfer of funds from platform to pilot
-      transfer_data: {
-        // Send the amount for the pilot after collecting a 20% platform fee:
-        // the `amountForPilot` method simply computes `ride.amount * 0.8`
-        amount: stripeTransaction.amount,
-        // The destination of this charge is the pilot's Stripe account
-        destination: receiver.stripeAccountId,
-      },
-    });
-    console.log('made it through charge')
-    // Add the Stripe charge reference to the ride and save it
-    stripeTransaction.stripeChargeId = charge.id;
-    stripeTransaction.save();
-  } catch (err) {
-    console.log(err);
-    // Return a 402 Payment Required error code
-    res.sendStatus(402);
-    next(`Error adding token to customer: ${err.message}`);
-  }
-  res.redirect('/pilots/dashboard');
-});
+// //generate a payment
+// router.post('/stripeTransaction', ensureAuthenticated, async (req, res, next) => {
+//   console.log('reached stripeTransaction');
+//   // const sender = req.user;
+//   // // Find a random passenger
+//   // const sender = await Sender.getRandom();
+//   // Create a new ride for the pilot and this random passenger
+//   console.log('receiver: ' + req.body.receiver);
+//   console.log('sender: '+ req.user.id);
+//   console.log('amount: '+ req.body.amount);
+//   let newStripeTransaction = new stripeTransaction({
+//     receiver: req.body.receiver,
+//     sender: req.user.id,
+//     // Generate a random amount between $10 and $100 for this ride
+//     amount: req.body.amount
+//   });
+//   console.log('made it through newStripeTransaction')
+//   // Save the ride
+//   await stripeTransaction(newStripeTransaction).save();
+//   try {
+//     // Get a test source, using the given testing behavior
+//     let source;
+//     if (req.body.immediate_balance) {
+//       source = getTestSource('immediate_balance');
+//     } else if (req.body.payout_limit) {
+//       source = getTestSource('payout_limit');
+//     }
+//     // Create a charge and set its destination to the pilot's account
+//     const charge = await stripe.charges.create({
+//       source: source,
+//       amount: stripeTransaction.amount,
+//       currency: stripeTransaction.currency,
+//       description: config.appName,
+//       statement_descriptor: config.appName,
+//       // The destination parameter directs the transfer of funds from platform to pilot
+//       transfer_data: {
+//         // Send the amount for the pilot after collecting a 20% platform fee:
+//         // the `amountForPilot` method simply computes `ride.amount * 0.8`
+//         amount: stripeTransaction.amount,
+//         // The destination of this charge is the pilot's Stripe account
+//         destination: receiver.stripeAccountId,
+//       },
+//     });
+//     console.log('made it through charge')
+//     // Add the Stripe charge reference to the ride and save it
+//     stripeTransaction.stripeChargeId = charge.id;
+//     stripeTransaction.save();
+//   } catch (err) {
+//     console.log(err);
+//     // Return a 402 Payment Required error code
+//     res.sendStatus(402);
+//     next(`Error adding token to customer: ${err.message}`);
+//   }
+//   res.redirect('/pilots/dashboard');
+// });
+// end of rocket rides template
 
 //the start of payment authentication using Fiverr Strategy
 const fee = 3.15;
