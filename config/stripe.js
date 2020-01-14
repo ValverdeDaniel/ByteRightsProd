31500030d88bf3d5d0f9d65fcfee96045811c949
unknown <Daniel@UNILAP17.baromahc.net>
2020-01-14 11:16:43 -0700

    updating stripe keys to live

--- config/stripe.js
+++ config/stripe.js
@@ -16,14 +16,23 @@ module.exports = {
   // Configuration for Stripe
   // API Keys: https://dashboard.stripe.com/account/apikeys
   // Connect Settings: https://dashboard.stripe.com/account/applications/settings
+  //test keys
+  // stripe: {
+  //   secretKey: 'sk_test_tF2ATjx0ybAcarTgqn0zpqbr005vSTX7PO',
+  //   publishableKey: 'pk_test_ggUprAz0qNKTol6UXU7bRtLu003bsXsePv',
+  //   clientId: 'ca_FtuG5UUMgotba2yf8gH1C6yQSnpr4pq1',
+  //   authorizeUri: 'https://connect.stripe.com/express/oauth/authorize',
+  //   tokenUri: 'https://connect.stripe.com/oauth/token'
+  // },
+
+  //live keys
   stripe: {
-    secretKey: 'sk_test_tF2ATjx0ybAcarTgqn0zpqbr005vSTX7PO',
-    publishableKey: 'pk_test_ggUprAz0qNKTol6UXU7bRtLu003bsXsePv',
-    clientId: 'ca_FtuG5UUMgotba2yf8gH1C6yQSnpr4pq1',
+    secretKey: 'sk_live_JDOuKnfK5EEsIjTxbpPer3dx00PaXr0hOs',
+    publishableKey: 'pk_live_PCXOaN7ilwRLSmzgninvaqp000yhPs2Se1',
+    clientId: 'ca_FtuGniH7a1hLRU0AnPkrYlFuh9vHMjuG',
     authorizeUri: 'https://connect.stripe.com/express/oauth/authorize',
     tokenUri: 'https://connect.stripe.com/oauth/token'
   },
-
   // // Configuration for MongoDB
   // mongoUri: 'mongodb://localhost/rocketrides',
 
--- routes/stripe.js
+++ routes/stripe.js
@@ -55,8 +55,8 @@ router.get('/authorize', ensureAuthenticated, (req, res) => {
   // and `phone` in the query parameters: those form fields will be prefilled
   parameters = Object.assign(parameters, {
     //previous redirect_uri config.publicDomain +
-    redirect_uri: 'localhost:5000/stripe/token',
-    //redirect_uri:'desolate-sierra-72554.herokuapp.com/stripe/token',
+    //redirect_uri: 'localhost:5000/stripe/token',
+    redirect_uri:'desolate-sierra-72554.herokuapp.com/stripe/token',
     'stripe_user[business_type]': req.user.type || 'individual',
     'stripe_user[business_name]': req.user.businessName || undefined,
     'stripe_user[first_name]': req.user.firstName || undefined,
@@ -149,8 +149,8 @@ router.get('/stripeDashboard', ensureAuthenticated, async (req, res) => {
       user.stripeAccountId, {
         //was pilots/dashboard
         //i want this to take me to stripeDashboard
-        redirect_url: req.session.redirectUrl ? req.session.redirectUrl : 'http://localhost:5000/dashboard'
-        //redirect_url: req.session.redirectUrl ? req.session.redirectUrl : 'https://desolate-sierra-72554.herokuapp.com/dashboard'
+        //redirect_url: req.session.redirectUrl ? req.session.redirectUrl : 'http://localhost:5000/dashboard'
+        redirect_url: req.session.redirectUrl ? req.session.redirectUrl : 'https://desolate-sierra-72554.herokuapp.com/dashboard'
       }
     );
     // Directly link to the account tab
