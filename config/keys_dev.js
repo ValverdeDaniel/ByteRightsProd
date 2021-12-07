39fb52c8c20c4aead9476907c0ca4894d26c7372
Daniel Valverde <valverdedaniel44@gmail.com>
2021-12-06 17:21:39 -0700

    updating to mongo atlas

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,6 +1,7 @@
 module.exports = {
-  mongoURI: 'mongodb://danny:danny123@ds253537.mlab.com:53537/brtest',
+  // mongoURI: 'mongodb://danny:danny123@ds253537.mlab.com:53537/brtest',
   //mongoURI: 'mongodb://danny123:danny123@ds037358.mlab.com:37358/brproddbm',
+  mongoURI: 'mongodb+srv://danny:danny123@cluster0.uogrp.mongodb.net/brtest?retryWrites=true&w=majority',
   googleClientID: '682997216522-arpf59hvmhjpcbgnv11lnshfa5mrtmma.apps.googleusercontent.com',
   googleClientSecret: 'cVLowijK8LQIviV5qkKzbx3P',
   facebookClientID: '1664378730362805',
